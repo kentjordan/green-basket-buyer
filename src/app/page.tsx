b30409553f1f84e5ee8015b357965da2cdd73540
Kent Jordan <kentaki.dev@gmail.com>
2024-04-11 19:30:01 +0800

    Added: background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import header_bg from "@/assets/header_bg.jpg";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <div className='w-full h-96 relative'>
+        <div className='w-full h-full z-10 bg-gradient-to-b from-black to-transparent absolute'></div>
+        <Image
+          src={header_bg}
+          alt='Food as Background'
+          className='w-full'
+          objectFit='cover'
+          layout='fill'
+        />
+      </div>
     </div>
   );
 }
